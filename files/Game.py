fd9bdebbe2d0d4cd6ea91c2661ede58ee7c39794
Unknown <lavapower84@gmail.com>
2018-05-01 23:00:36 +0200

    Add items in inventory and show their

--- files/Game.py
+++ files/Game.py
@@ -1,30 +1,33 @@
 import pygame, random
 from PIL import Image
 try:
-    from files.Class import Monika, Yuri, Natsuki, Sayori
+    from files.Class import Monika, Yuri, Natsuki, Sayori, Cookie, Soda, JusPomme, The, Cupcake, Gateau
 except ImportError:
     from Class import Monika, Yuri, Natsuki, Sayori
 
 class Game():
     def __init__(self, girlChoosed):
+        pygame.init()
         if girlChoosed == "Monika":
-            self.girl = Monika()
+            girl = Monika()
         elif girlChoosed == "Yuri":
-            self.girl = Yuri()
+            girl = Yuri()
         elif girlChoosed == "Sayori":
-            self.girl = Sayori()
+            girl = Sayori()
         elif girlChoosed == "Natsuki":
-            self.girl = Natsuki()
+            girl = Natsuki()
         else:
             raise Exception
         self.girlGroup = pygame.sprite.Group()
-        self.girlGroup.add(self.girl)
+        self.girlGroup.add(girl)
         self.timeDirection = 20
+        self.inventory = [Cookie(), The(), Cupcake(), JusPomme(), Gateau(), Soda()]
+        self.font=pygame.font.SysFont("Times New Roman",22,bold=True,italic=False)
         self.played = True
         self.screen = pygame.display.set_mode((500, 500))
 
         self.clock = pygame.time.Clock()
-        pygame.display.set_caption("DDLC's Friends - "+self.girl.name)
+        pygame.display.set_caption("DDLC's Friends - "+girl.name)
 
     def launch(self):
         while self.played:
@@ -45,6 +48,9 @@ class Game():
                 self.screen.blit(pygame.transform.scale(content, (i.fun, 6)), (25, 51))
                 self.screen.blit(pygame.transform.scale(content, (i.faim, 6)), (411+(65-i.faim), 21))
                 self.screen.blit(pygame.transform.scale(content, (i.soif, 6)), (411+(65-i.faim), 51))
+                text=self.font.render(i.name,1,(250, 240, 230))
+                text_rect = text.get_rect(center=(500/2, 20))
+                self.screen.blit(text, text_rect)
             frame = pygame.image.load("files/images/frame.png")
             self.screen.blit(pygame.transform.scale(frame, (500, 70)), (0,430))
             self.screen.blit(pygame.transform.scale(frame, (500, 70)), (0,360))
@@ -52,6 +58,10 @@ class Game():
             self.screen.blit(pygame.image.load("files/images/barre_fun.png"), (10, 40))
             self.screen.blit(pygame.image.load("files/images/barre_faim.png"), (406, 10))
             self.screen.blit(pygame.image.load("files/images/barre_soif.png"), (406, 40))
+            for i in self.inventory:
+                self.screen.blit(i.image, (i.rect.x, i.rect.y))
+                text=self.font.render(str(i.nombre),1,(0, 0,0))
+                self.screen.blit(text, (i.rect.x + i.offsetTX, i.rect.y + i.offsetTY))
             self.clock.tick(60)
             pygame.display.update()
         pygame.quit()
